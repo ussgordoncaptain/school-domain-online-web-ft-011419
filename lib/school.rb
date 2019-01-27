8200ab8423ca5fe3e1f05284600f2acb829a5f9c
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-27 16:10:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+ attr_accessor :roster, :name
+  def add_student(student, grade_level)
+    if @roster.has_key?(grade_level)
+      @roster[grade_level] << student
+    else
+      @roster[grade_level] = [student]
+    end
+  end
+  def grade(grade_level)
+    @roster[grade_level]
+  end
+  def sort 
+    roster.each do |grade_level, students|
+      students.sort!
+    end
+  end
+end
